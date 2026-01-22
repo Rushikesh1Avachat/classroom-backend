8294fdc15530414eb2019c88723487b080629559
Rushikesh1 Avachat <avachatrushikesh45@gmail.com>
2026-01-22 16:51:17 +0530

    fix render issue

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,23 @@
-
-
 import('apminsight')
     .then(({ default: AgentAPI }) => AgentAPI.config())
     .catch(() => console.log('APM not available in this environment'));
-import express from "express";
-import subjectsRouter from "./routes/subjects.js";
+
 import cors from "cors";
-import {toNodeHandler} from "better-auth/node";
-import securityMiddleware from "./middleware/security.js";
-import {auth} from "./lib/auth.js";
+import express from "express";
+import { toNodeHandler } from "better-auth/node";
+
+import subjectsRouter from "./routes/subjects.js";
+// import usersRouter from "./routes/users.js";
+import classesRouter from "./routes/classes.js";
+// import departmentsRouter from "./routes/departments.js";
+// import statsRouter from "./routes/stats.js";
+// import enrollmentsRouter from "./routes/enrollments.js";
+
+// import securityMiddleware from "./middleware/security.js";
+import { auth } from "./lib/auth.js";
 
 const app = express();
 const PORT = 8000;
-
-
-app.all('/api/auth/*splat', toNodeHandler(auth));
-app.use(express.json());
-app.use(securityMiddleware);
-
 
 app.use(
     cors({
@@ -26,15 +26,24 @@ app.use(
         credentials: true, // allow cookies
     })
 );
+
+app.all("/api/auth/*splat", toNodeHandler(auth));
+
+app.use(express.json());
+
+// app.use(securityMiddleware);
+
 app.use("/api/subjects", subjectsRouter);
-
-
-
+// app.use("/api/users", usersRouter);
+app.use("/api/classes", classesRouter);
+// app.use("/api/departments", departmentsRouter);
+// app.use("/api/stats", statsRouter);
+// app.use("/api/enrollments", enrollmentsRouter);
 
 app.get("/", (req, res) => {
-  res.send("Backend server is running!");
+    res.send("Backend server is running!");
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+    console.log(`Server running at http://localhost:${PORT}`);
 });
